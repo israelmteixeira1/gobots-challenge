ba6abd63c9ee3064eff6f566d72783c857da8a58
Israel Teixeira <israelmteixeira1@gmail.com>
2025-12-26 14:54:49 -0300

    feat: adiciona infraestrutura básica com Docker e MongoDB: - cria Dockerfile para marketplace-api e receiver-api - adiciona docker-compose para orquestração do ambiente - adiciona configuração inicial para o MongoDB

--- marketplace-api/src/app.module.ts
+++ marketplace-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
