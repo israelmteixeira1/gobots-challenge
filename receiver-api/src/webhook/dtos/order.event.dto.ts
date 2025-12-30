102738b413c035500c40067c0c45cbd42915e6e5
Israel Teixeira <israelmteixeira1@gmail.com>
2025-12-29 21:58:41 -0300

    feat: adiciona validação no evento recebido via webhook no receiver-api

--- receiver-api/src/webhook/dtos/order.event.dto.ts
+++ receiver-api/src/webhook/dtos/order.event.dto.ts
@@ -1,9 +1,22 @@
 import { OrderEvent } from '../enums/order-event.enum';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class OrderEventDto {
+  @IsString()
+  @IsNotEmpty()
   eventId: string;
+
+  @IsEnum(OrderEvent)
   event: OrderEvent;
+
+  @IsString()
+  @IsNotEmpty()
   orderId: string;
+
+  @IsString()
+  @IsNotEmpty()
   storeId: string;
+
+  @IsString()
   timestamp: string;
 }
